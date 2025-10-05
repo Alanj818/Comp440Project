ee4b997bfd8ab36d8a2bf64ddcdb4186ffca4bf2
chickensPG <videsh.shrestha@gmail.com>
2025-10-04 19:28:28 -0700

    Created backend folder, setting up flask api, set up db_init file to be used

---
+++ src/backend/app.py
@@ -0,0 +1,61 @@
+#---------------------Libraries and packages here---------------------------#
+
+
+"""
+flask -> API framework, importing Flask itself, session to save user token, request to handle HTTP request within the webpage itself
+
+requests -> handles HTTP requests for pages outside the webpage
+
+json -> just in case json needs to be serialized/deserialized
+
+psycopg2 -> Postgres database manipulation library
+
+flask_bcrypt -> bcrypt password hashing for flask
+"""
+
+from flask import Flask, session, request
+import requests, json
+import psycopg2 as pg
+from flask_bcrypt import Bcrypt
+
+#----------------------------------------------------------------------------#
+
+
+from .db_init import connect_db
+
+
+
+# Initializing app and bcrypt
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+
+db = connect_db()
+
+
+# Checks if login is valid 
+def valid_login(u, p):
+    username = u
+    password = p
+    if(bcrypt.check_password_hash(password))
+
+
+@app.route("/signup", methods = ['GET', 'POST'])
+def signup():
+     if request.method == 'POST':
+
+
+
+
+
+
+
+
+
+# Login route, methods determines what HTTP functions that this page will use
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+  #  if request.method == 'POST':
+      
+   
+
