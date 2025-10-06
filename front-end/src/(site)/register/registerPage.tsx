799d8e7e04911ab5f9fc8eed71bc4336198f8c23
Will159c <163231055+Will159c@users.noreply.github.com>
2025-10-06 12:11:46 -0700

    Added login and register page without functionality

---
+++ front-end/src/(site)/register/registerPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterComponent from '../../components/register/register';
+
+export default function RegisterPage() {
+    return (
+      <RegisterComponent />
+    );
+}
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RegisterPage from './(site)/register/registerPage';
+import LoginPage from './(site)/login/loginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<h1>Home Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
